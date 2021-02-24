464208c0b44b01ed104729ca7b8c027f62030582
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-02-24 19:26:15 -0300

    exercicio 4 realizado

---
+++ modulo_1/bloco_4/dia_1/introducao-a-javascript-e-logica-de-programacao_1/exercicio_5/script.js
@@ -0,0 +1,11 @@
+let angulo1 = 60;
+let angulo2 = 60;
+let angulo3 = 60;
+
+let resultadoTriangulo = angulo1 + angulo2 + angulo3
+
+if (resultadoTriangulo >= 180) {
+    console.log(true);
+} else {
+    console.log(false)
+}

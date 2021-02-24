137a6caddfcbc694ac51fda54409b501a5791961
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-02-24 18:53:23 -0300

    exercicio 2 realizado

---
+++ modulo_1/bloco_4/dia_1/introducao-a-javascript-e-logica-de-programacao_1/exercicio_2/script.js
@@ -0,0 +1,8 @@
+let xablau = 1000;
+let xablauzinho = 1500;
+
+if(xablau > xablauzinho){
+    console.log("xablau é maior que xablauzinho")
+}else{
+    console.log("xablau não é maior que xablauzinho")
+}

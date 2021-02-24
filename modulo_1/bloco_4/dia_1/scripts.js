b70d2751a9453684c10bd89513d7ca619b7388af
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-02-24 18:11:13 -0300

    criando pasta do bloco 4

---
+++ modulo_1/bloco_4/dia_1/scripts.js
@@ -0,0 +1,9 @@
+// let notoCandidata = 55;
+
+// if (notoCandidata > 80) 
+//     console.log("Parabens, você foi aprovada(a)", );
+// else if  (notoCandidata < 80 && notoCandidata > 60) 
+//     console.log("Você está na nossa lista de espera")
+// else { 
+//     console.log(" Você foi reprovada(o)");
+// }

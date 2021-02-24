87568d4e6745c05ae678b2ed76bcef5ff91a6e2a
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-02-24 19:00:11 -0300

    pasta exercicio 3 criada

---
+++ modulo_1/bloco_4/dia_1/introducao-a-javascript-e-logica-de-programacao_1/exercicio_3/script.js
@@ -0,0 +1,11 @@
+let var1 = 30; 
+let var2 = 50;
+let var3 = 10;
+
+if(var1 > var2 && var1 > var3 ){
+    console.log("var1 é a maior variavel")
+} else if (var2 > var1 && var2 > var3) {
+    console.log("var2 é a maior variavel")
+} else {
+    console.log("var3 é a maior variavel")
+}

acafd9adb9c194c07e21d1fb2139bf810675e22d
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-02-25 15:38:42 -0300

    exercicios de manipulacao de arrays realizado

---
+++ modulo_1/bloco_4/dia_2/array_e_for_em_javascript/scriptExercicioConteudo.js
@@ -0,0 +1,51 @@
+
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa ao final do array
+// tasksList.unshift('Fazer exercícios da Trybe');  // adiciona mais uma tarefa ao inicio do array
+// console.log(tasksList);
+
+// // ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.pop();  // remove a última tarefa
+// tasksList.shift();
+// console.log(tasksList);
+// [ 'Tomar café', 'Reunião' ]
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// let indexOfTask = tasksList.indexOf('Reunião');
+// console.log(indexOfTask);
+
+// 1
+
+//exercicio de fixação do conteudo -------------------------------------------------------
+
+// exercicio 1
+//Obtenha o valor "Serviços" do array menu :
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+//exercicio 2
+
+//Procure o índice do valor "Portfólio" do array menu :
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio') ;
+
+// console.log(indexOfPortfolio);
+
+// EXERCICIO 3
+
+//Adicione o valor "Contato" no final do array menu :
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato')
+
+// console.log(menu);

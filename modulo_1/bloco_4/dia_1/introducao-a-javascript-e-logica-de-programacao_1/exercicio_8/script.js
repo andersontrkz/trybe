d75ba26452d91daf8f35a475db4e496563c17bd6
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-02-26 04:10:11 -0300

    exercicios 7 8 9 10 11 realizados

---
+++ modulo_1/bloco_4/dia_1/introducao-a-javascript-e-logica-de-programacao_1/exercicio_8/script.js
@@ -0,0 +1,10 @@
+let value1 = 7;
+let value2 = 7;
+let value3 = 5;
+let isEven = false;
+
+if ((value1 % 2 === 0 || value2 % 2 === 0 || value3 % 3 === 0 )) {
+    isEven = true;
+}
+
+console.log(isEven);

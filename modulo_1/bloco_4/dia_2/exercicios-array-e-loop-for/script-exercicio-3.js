05775903f635b5b2436f8d25d033ab0dd36394f7
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-02-26 02:40:05 -0300

    exercicio 4, 5, 6, 7, 8, 9 realizados

--- modulo_1/bloco_4/dia_2/exercicios-array-e-loop-for/script-exercicio-3.js
+++ modulo_1/bloco_4/dia_2/exercicios-array-e-loop-for/script-exercicio-3.js
@@ -1,3 +1,4 @@
+//exercicio 3
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let average = 0;
---
+++ modulo_1/bloco_4/dia_2/exercicios-array-e-loop-for/script-exercicio-4.js
@@ -0,0 +1,78 @@
+// exercicio 4
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let average = 0;
+// let sum = 0;
+
+//exercicio 4
+
+//somando em sum os numeros do array
+// for (let indexAvarage = 0; indexAvarage < numbers.length; indexAvarage += 1) {
+//     sum += numbers[indexAvarage]
+// }
+// average = sum / numbers.length
+
+// if (average > 20) {
+//     console.log('valor maior que 20')
+// } else {
+//     console.log('valor menor que 20')
+// }
+
+// exercicio 5 
+
+// let value = 0;
+
+// for (let indexValue = 0; indexValue < numbers.length; indexValue += 1) {    
+//     if(value < numbers[indexValue]){
+//         value = numbers[indexValue];
+//     }
+// }
+// console.log(`o maior valor do array é ${value}`);
+
+//exercicio 6
+
+
+// let evenValues = 0;
+// let value = 0;
+// let error;
+
+// for (let indexValues = 0; indexValues < numbers.length; indexValues += 1) {    
+//     if(numbers[indexValues] % 2 !== 0 ){
+//        evenValues += 1;
+//     } 
+// }
+
+// if (evenValues === 0){
+//     console.log('nenhum valor impar encontrado');
+// } else {
+//     console.log(evenValues);
+// }
+
+
+// exercicio 7
+
+// let smallestNumber = 1000;
+
+// for (let indexValue = 0; indexValue < numbers.length; indexValue += 1) {    
+//     if (numbers[indexValue] < smallestNumber) {
+//         smallestNumber = numbers[indexValue];
+//     };
+// }
+// console.log(smallestNumber);
+
+
+// exercicio 8 
+
+let numbers = [];
+
+for (let indexArray = 1 ; indexArray < 25; indexArray += 1) {
+    numbers.push(indexArray);
+}
+console.log(numbers);
+
+//exercicio 9
+
+for (let indexDiv = 0; indexDiv < numbers.length; indexDiv += 1) {
+   numbers[indexDiv] = numbers[indexDiv] /2;
+}
+console.log(numbers);

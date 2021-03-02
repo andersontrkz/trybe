d2741ff756f1aba5be116779ae66fbce5ac9f773
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-03-02 13:46:40 -0300

    Exercicio 3 realizado

--- introducao-a-Objetos-e-Funcoes/scripts_exercises_part2.js
+++ introducao-a-Objetos-e-Funcoes/scripts_exercises_part2.js
@@ -1,14 +1,44 @@
 // Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
 
-function verificaPalindrome(string){
-    let arrayReverse = string.split("").reverse().join("");
-    if (arrayReverse === string){
-        return true;
-    } else {
-        return false;
-    }
-};
-console.log(verificaPalindrome('arara'));
-console.log(verificaPalindrome('desenvolvimento'));
+// function verificaPalindrome(string){
+//     let arrayReverse = string.split("").reverse().join("");
+//     if (arrayReverse === string){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
+// console.log(verificaPalindrome('arara'));
+// console.log(verificaPalindrome('desenvolvimento'));
 
+//2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
+// let arrayTeste = [2, 3, 6, 7, 10, 1];
+
+// function indiceDoMaior(numeros) {
+//     let indiceMaior = 0;
+//     for (let indice in numeros) {
+//       if (numeros[indiceMaior] < numeros[indice]) {
+//         indiceMaior = indice;
+//       }
+//     }
+//     return indiceMaior;
+//   }
+  
+//   console.log(indiceDoMaior([2, 3, 6, 7, 10, 1])); //4
+
+// 3 Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+// function indiceDoMenor(numeros) {
+//     let indiceMenor = 0;
+//     for (let indice in numeros) {
+//       if (numeros[indiceMenor] > numeros[indice]) {
+//         indiceMenor = indice;
+//       }
+//     }
+//     return indiceMenor;
+//   }
+  
+//   console.log(indiceDoMenor([2, 4, 6, 7, 10, 0, -3])); //6
+
+//4- Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.

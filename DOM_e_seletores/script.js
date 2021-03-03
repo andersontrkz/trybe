e079e709711157ec2574a5944c822ee931603f1c
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-03-03 18:35:19 -0300

    Estruturando o diretorio para fazer os exercicios

---
+++ DOM_e_seletores/script.js
@@ -0,0 +1,10 @@
+//recuperando o titulo
+var paragraph = document.getElementById("page-title");
+paragraph.innerText = "Vingadores Ultimato"; 
+
+var paragraph = document.getElementById("second-paragraph");
+paragraph.style.backgroundColor = "blue"; 
+
+var paragraph = document.getElementById("subtitle");
+paragraph.style.backgroundColor = "green"; 
+

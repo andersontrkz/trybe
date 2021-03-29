cda223bd67b495530d2c7dd1072a3539f3e966f3
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-03-29 14:58:35 -0300

    ainda falta realizar alguns exercicios

--- Testes_unitarios_em_JavaScript/script-teste2.js
+++ Testes_unitarios_em_JavaScript/script-teste2.js
@@ -14,12 +14,15 @@ function myRemove(arr, item) {
 const result = myRemove([1, 2, 3, 4], 3)
 
 
- assert.notDeepStrictEqual(result, [1, 2, 3, 4]);
+ assert.deepStrictEqual(result, [1, 2, 4]);
 
 
 //A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
 
 // Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+
 //  Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]  => assert.notDeepStrictEqual(result, [1, 2, 3, 4]);
+
+
 // Verifique se o array passado por parâmetro não sofreu alterações
 // Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado

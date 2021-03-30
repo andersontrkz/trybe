dab09efd1957c55c1e2a98c5ad6f4dd681a3812f
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-03-30 18:56:07 -0300

    exemplos de conteudos realizados

--- HOF_forEach_find_some_every_sort/script-conteudo.js
+++ HOF_forEach_find_some_every_sort/script-conteudo.js
@@ -1,17 +1,64 @@
-const emailListInData = [
-  'roberta@email.com',
-  'paulo@email.com',
-  'anaroberta@email.com',
-  'fabiano@email.com',
-  'Lukas11.rs@hotmail.com'
-];
+// const emailListInData = [
+//   'roberta@email.com',
+//   'paulo@email.com',
+//   'anaroberta@email.com',
+//   'fabiano@email.com',
+//   'Lukas11.rs@hotmail.com'
+// ];
 
-const showEmailList = (email) => {
-  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
-};
+// const showEmailList = (email) => {
+//   console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+// };
 
-// Use o método forEach chamando a callback showEmailList para apresentar os emails.
-emailListInData.forEach((Element) => {
-  showEmailList(Element);
-});
-emailListInData.forEach((Element) => showEmailList(Element));// versão resumida do mesmo codigo
+// // Use o método forEach chamando a callback showEmailList para apresentar os emails.
+// emailListInData.forEach((Element) => {
+//   showEmailList(Element);
+// });
+// emailListInData.forEach((Element) => showEmailList(Element));// versão resumida do mesmo codigo
+
+//-----------------------------------------------------------------------------------------------------------------------
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const multipliesFor2 = (element) => {
+//   console.log(`${element} * 2: ${element * 2}`);
+// };
+
+// numbers.forEach(multipliesFor2);
+
+//----------------------------------------------------------------------------------------------------------------------
+// const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+// const convertToUpperCase = (name, index) => {
+//   names[index] = name.toUpperCase();
+// };
+
+// names.forEach(convertToUpperCase);
+// console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
+// -------------------------------------------------------------------------------------------------------------------
+
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// const verifyEven = (number) => number % 2 === 0;
+
+// const isEven = numbers.find(verifyEven);
+
+// console.log(isEven); // 30
+
+// console.log(verifyEven(9)); // False
+// console.log(verifyEven(14)); // True
+
+// // Outra forma de ser realizada sem a necessidade de criar uma nova função.
+// const isEven2 = numbers.find((number) => number % 2 === 0);
+
+// console.log(isEven2); // 30
+// -------------------------------------------------------------------------------------------------------
+
+// O exemplo abaixo usa o some para verificar se possui algum nome que começa com a letra desejada:
+const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+console.log(verifyFirstLetter('J', listNames)); // true
+console.log(verifyFirstLetter('x', listNames)); // false
+ 
+//O exemplo abaixo usará o every para verificar se o estudante passou em todas as matérias:

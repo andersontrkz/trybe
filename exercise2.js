4e900e8744ec325949a3c11c46f606b817c75a6c
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-05 18:43:03 -0300

    exercicio 2 realizado

--- exercise2.js
+++ exercise2.js
@@ -1,5 +1,3 @@
-//Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
-// Dica: use as funções map , sort
 const assert = require('assert');
 
 const books = [
@@ -92,8 +90,32 @@ const expectedResult = [
   },
 ];
 
+//Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+// Dica: use as funções map , sort
+
+//função para ordenação por idade que autor tinha no lançamento do livro---------------------
 function nameAndAge() {
-  // escreva seu código aqui
+  function compare(a,b) {
+    if (a.age < b.age)
+       return -1;
+    if (a.age > b.age)
+      return 1;
+    return 0;
+  }
+
+  //construção do objeto propriamente dito 
+  const objReturn = books.map((book) => {
+    const releaseYear = book.releaseYear;
+    const ageAuthor = book.author.birthYear;
+    const ageAuthorLaunch = releaseYear - ageAuthor;
+    const newObjt = {
+      age: ageAuthorLaunch,
+      author: book.author.name
+    }
+    return newObjt;
+  });
+  return objReturn.sort(compare);
 }
+console.log(nameAndAge());
 
 assert.deepStrictEqual(nameAndAge(), expectedResult);

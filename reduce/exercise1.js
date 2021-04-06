b4f61f8243eea7cf05dba8258ae6076900ca5f46
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-06 15:38:40 -0300

    Estrutura dos exercicios do dia criada

---
+++ reduce/exercise1.js
@@ -0,0 +1,15 @@
+
+const assert = require('assert');
+
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+];
+
+// Dada uma matriz de matrizes, transforme em uma única matriz.
+function flatten() {
+  // escreva seu código aqui
+}
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);

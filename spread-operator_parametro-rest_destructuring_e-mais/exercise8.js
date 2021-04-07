7f15690aaeb923f02cdf242749de3f726648a918
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-07 18:42:04 -0300

    Estruturas dos exerccios realizadas

---
+++ spread-operator_parametro-rest_destructuring_e-mais/exercise8.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+// Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+// Dica: use default params .
+
+// escreva greet abaixo
+
+assert.strictEqual(greet('John'), 'Hi John');
+assert.strictEqual(greet('John', 'Good morning'), 'Good morning John');
+assert.strictEqual(greet('Isabela', 'Oi'), 'Oi Isabela');

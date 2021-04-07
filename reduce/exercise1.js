a95849f3b5866ff841beead33ff3694635afa790
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-06 22:51:45 -0300

    Exercicio 1 realizado

--- reduce/exercise1.js
+++ reduce/exercise1.js
@@ -8,8 +8,11 @@ const arrays = [
 ];
 
 // Dada uma matriz de matrizes, transforme em uma única matriz.
-function flatten() {
-  // escreva seu código aqui
-}
 
-assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
+function flatten() {
+  return arrays.reduce((acc, curr) => acc.concat(curr), [])
+}
+console.log(flatten());
+
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6])

d39c27259e47b1d16f9bc77cce0841ecff5ba9c9
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-07 18:35:26 -0300

    exemplos de conteudo testados

--- spread-operator_parametro-rest_destructuring_e-mais/scriptExemplosConteudo.js
+++ spread-operator_parametro-rest_destructuring_e-mais/scriptExemplosConteudo.js
@@ -1,3 +1,4 @@
+//SPREAD OPERATOR=============================================================================
 // Faça uma lista com as suas frutas favoritas
 const specialFruit = ['Banana', 'Morango', 'Maracuja'];
 
@@ -10,3 +11,52 @@ const fruitSalad = (fruit, additional) => {
 };
 
 console.log(fruitSalad(specialFruit, additionalItens));
+
+
+// PARAMETRO REST=============================================================================
+const sum = (...args) => args.reduce((accumulator, current) => accumulator + current, 0);
+console.log(sum(4, 7, 8, 9, 60, 1)); // 88
+
+const person = {
+  name: 'João',
+  lastName: 'Jr',
+  age: 34,
+};
+
+const { lastName } = person;
+console.log(lastName);
+
+//====================================================================================================
+const getNationality = ({ firstName, nationality = 'brazil' }) => `${firstName} is ${nationality}`;
+
+const person = {
+    firstName: 'João',
+    lastName: 'Jr II',
+};
+
+
+
+const otherPerson = {
+    firstName: 'Ivan',
+    lastName: 'Ivanovich',
+    nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
+
+
+
+// Object property shorthand=============================================================================
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude});
+
+console.log(getPosition(-19.8157, -43.9542));
+
+
+//default parameters =============================================================================
+
+const greeting = (user = 'usuário') => console.log(`Welcome ${user}!`);
+
+greeting(); // // Welcome usuário!

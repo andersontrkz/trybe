78db8dcb7d21e76c64161d1cc1078d0afc0fba06
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-08 00:44:22 -0300

    Exercicios 6 e 7 realizados

--- spread-operator_parametro-rest_destructuring_e-mais/exercise6.js
+++ spread-operator_parametro-rest_destructuring_e-mais/exercise6.js
@@ -5,9 +5,13 @@ const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
 // Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array. Porém, essa modelagem está baixo nível. Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+
 // Dica: use array destructuring e abbreviation object literal .
 
 // escreva toObject abaixo
+
+const toObject = ([name, brand, year]) => ({name, brand, year});
+
 
 assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
 assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
--- spread-operator_parametro-rest_destructuring_e-mais/exercise7.js
+++ spread-operator_parametro-rest_destructuring_e-mais/exercise7.js
@@ -1,6 +1,3 @@
-// Escreva uma função shipLength que, dado um objeto representando um navio, retorna o comprimento dele, mostrando também a devida unidade de comprimento:
-// Dica: use object destructuring .
-
 const assert = require('assert');
 
 const ships = [
@@ -21,7 +18,13 @@ const ships = [
     },
 ];
 
+// Escreva uma função shipLength que, dado um objeto representando um navio, retorna o comprimento dele, mostrando também a devida unidade de comprimento:
+// Dica: use object destructuring .
+
 // escreva shipLength abaixo
+
+const shipLength = ({name, length, measurementUnit}) => `${name} is ${length} ${measurementUnit} long`;
+
 
 assert.strictEqual(shipLength(ships[0]), 'Titanic is 269.1 meters long');
 assert.strictEqual(shipLength(ships[1]), 'Queen Mary 2 is 1132 feet long');

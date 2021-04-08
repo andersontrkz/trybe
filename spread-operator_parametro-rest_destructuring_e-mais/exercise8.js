5182e40a545b5834119458a9deda25ba747109c3
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-08 01:03:54 -0300

    Exercicio 8 realizado

--- spread-operator_parametro-rest_destructuring_e-mais/exercise8.js
+++ spread-operator_parametro-rest_destructuring_e-mais/exercise8.js
@@ -4,6 +4,8 @@ const assert = require('assert');
 
 // escreva greet abaixo
 
+const greet = (nome, cumprimento = 'Hi') => `${cumprimento} ${nome}`;
+
 assert.strictEqual(greet('John'), 'Hi John');
 assert.strictEqual(greet('John', 'Good morning'), 'Good morning John');
 assert.strictEqual(greet('Isabela', 'Oi'), 'Oi Isabela');

f2cff65d7a561bfa8b3da8c0a61c2928afc66391
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-19 16:02:59 -0300

    .

--- promises/apiScript.js
+++ promises/apiScript.js
@@ -1,15 +1,36 @@
-// apiScript.js
+// apiScript.js     
+const fetch = require('node-fetch')
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
   const myObject = {
     method: 'GET',
-    Headers:{ 'Accept': 'application/json'}
-  }
+    headers: { 'Accept': 'application/json' }
+  };
 
   fetch(API_URL, myObject)
-    .then(response => console.log(response))
+    .then(response => response.json())
+    .then(data => console.log(data));
 };
 
-window.onload = () => fetchJoke();
+
+const fetchPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+   
+    const myArray = Array.from(
+      { length:10 },
+      () => Math.floor(Math.random() * 50) + 1,)
+
+    const sum = myArray.map(number => number * number)
+    .reduce((sum, number) => sum + number);
+    
+    sum < 8000 ? resolve(sum) : reject();
+  })
+
+  myPromise
+  .then(sum => [2, 3, 5, 10].map((number) => sum / number))
+  .then(array => array.reduce((number, acc) => number + acc, 0))
+  .catch(()=> console.log('É mais de oito mil! Essa promise deve estar quebrada!'))
+}
+
+fetchPromise();
